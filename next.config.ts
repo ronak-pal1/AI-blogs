b1cdacbeda088c84daabfacb861aa862534deba6
ronak-pal1 <ronakpaul882@gmail.com>
2025-08-28 19:02:26 +0530

    deployment fix

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  
+  experimental:{
+    optimizeCss:false,
+  }
 };
 
 export default nextConfig;
