ef990bb72def42aa863ed48345117972eeb8d3a5
ronak-pal1 <ronakpaul882@gmail.com>
2025-08-28 18:29:07 +0530

    Responsiveness added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,61 +6,61 @@ export default function Home() {
   return (
     <div className="w-full h-screen flex flex-1 flex-col items-center justify-center relative z-10">
 
-      <div className="w-full h-full flex-[0.15] flex">
-        <div className="w-full h-full flex-[0.2]"></div>
-        <div className="w-full h-full flex-[0.8]  border-r border-l border-[#e5cbba] flex items-center bg-background">
-          <div className="w-full flex items-center justify-between mx-4 my-5">
+      <div className="w-full h-full flex-[0.1] sm:flex-[0.15] flex">
+        <div className="w-full h-full flex-[0] sm:flex-[0.2]"></div>
+        <div className="w-full h-full flex-1 sm:flex-[0.8]  border-r border-l border-[#e5cbba] flex items-center bg-background">
+          <div className="w-full flex items-center justify-between mx-2 sm:mx-4 my-5">
             <div className="space-y-1">
-              <h1 className="text-2xl font-semibold ">AL/ML Blogs <span className="text-[9px] text-gray-500 font-light">by Ronak Paul</span> </h1>
-              <p className="text-xs text-gray-500">Last updated: 28 Aug 2025</p>
+              <h1 className="text-lg sm:text-2xl font-semibold ">Blogs <span className="text-[7px] sm:text-[9px] text-gray-500 font-light">by Ronak Paul</span> </h1>
+              <p className="text-[8px] sm:text-xs text-gray-500">Last updated: 28 Aug 2025</p>
             </div>
 
             <div className="flex flex-col items-end space-y-3">
-            <div className="flex items-center space-x-7">
+            <div className="flex items-center space-x-7 text-[8px] sm:text-xs">
               <Link
                 href="https://github.com/ronak-pal1"
-                className="text-xs text-blue-500 flex items-center space-x-2"
+                className=" text-blue-500 flex items-center space-x-2"
               >
                 GitHub
               </Link>
 
               <Link
                 href="https://linkedin.com/in/ronak-pal1"
-                className="text-xs text-blue-500 flex items-center space-x-2"
+                className=" text-blue-500 flex items-center space-x-2"
               >
                 LinkedIn
               </Link>
 
               <Link
                 href="https://x.com/ronak_pal1"
-                className="text-xs text-blue-500 flex items-center space-x-2"
+                className=" text-blue-500 flex items-center space-x-2"
               >
                 X
               </Link>
 
               <Link
                 href="https://ronakpaul.com"
-                className="text-xs text-blue-500 flex items-center space-x-2"
+                className=" text-blue-500 flex items-center space-x-2"
               >
                 Portfolio
               </Link>
             </div>
 
-            <div className="flex items-center space-x-3 py-2 px-3 border border-[#e5cbba] rounded-full">
-              <CiSearch className="text-xs"/>
-              <input type="text" placeholder="Search" className=" w-[300px] text-xs outline-none border-none"/>
+            <div className="flex items-center space-x-3  py-1 sm:py-2 px-2 sm:px-3 border border-[#e5cbba] rounded-full">
+              <CiSearch className="text-[9px] sm:text-xs"/>
+              <input type="text" placeholder="Search" className="w-[150px] md:w-[300px] text-[9px] sm:text-xs outline-none border-none"/>
             </div>
             </div>
            
           </div>
         </div>
-        <div className="w-full h-full flex-[0.2]"></div>
+        <div className="w-full h-full flex-[0] sm:flex-[0.2]"></div>
       </div>
 
-      <div className="w-full h-full flex-[0.85] flex border-t border-[#e5cbba] overflow-y-hidden">
-        <div className="w-full h-full flex-[0.2]"></div>
-        <div className="w-full h-full flex-[0.8] border-r border-l border-[#e5cbba] flex flex-col overflow-y-scroll custom-scrollbar bg-background">
-          <div className="flex flex-col space-y-10 mx-4 my-5">
+      <div className="w-full h-full flex-[0.9] sm:flex-[0.85] flex border-t border-[#e5cbba] overflow-y-hidden">
+        <div className="w-full h-full flex-[0] sm:flex-[0.2]"></div>
+        <div className="w-full h-full flex-1 sm:flex-[0.8] border-r border-l border-[#e5cbba] flex flex-col overflow-y-scroll custom-scrollbar bg-background">
+          <div className="flex flex-col space-y-5 sm:space-y-10 mx-2 sm:mx-4 my-5">
             <BlogCard
               title="AI bubble is bursting"
               subtitle="AI is not a panacea. More hype, less action. It's time to focus on the fundamentals. So, let's get started."
@@ -123,7 +123,7 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div className="w-full h-full flex-[0.2]"></div>
+        <div className="w-full h-full flex-[0] sm:flex-[0.2]"></div>
       </div>
     </div>
   );
--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -10,34 +10,34 @@ interface BlogCardProps {
 
 const BlogCard = ({ title, subtitle, date }: BlogCardProps) => {
   return (
-    <div className="w-full h-full flex flex-col space-y-2 hover:bg-[#e5cbbaa7] py-5 px-4 cursor-pointer">
+    <div className="w-full h-full flex flex-col space-y-2 hover:bg-[#e5cbbaa7] py-5 px-2 sm:px-4 cursor-pointer">
       <div className="flex items-center justify-between">
         <div className="flex items-center space-x-2">
-          <h2 className="text-lg font-semibold">{title}</h2>
+          <h2 className="text-base sm:text-lg font-semibold">{title}</h2>
           <Link
             href={"/blog/aeahe"}
-            className="text-xs text-blue-500 flex items-center space-x-2"
+            className="text-[9px] sm:text-xs text-blue-500 flex items-center space-x-2"
           >
             Open
             <GoArrowUpRight size={12} />
           </Link>
         </div>
-        <small className="text-xs text-gray-500">{date}</small>
+        <small className="text-[9px] sm:text-xs text-gray-500">{date}</small>
       </div>
-      <p className="text-xs text-gray-500">{subtitle}</p>
+      <p className="text-[9px] sm:text-xs text-gray-500">{subtitle}</p>
 
       <div className="flex items-center justify-between">
         <div className="w-full flex items-center space-x-4 mt-3">
           {["AI", "startups", "MCP"].map((tag) => (
             <span
               key={tag}
-              className="text-[9px] text-gray-500 px-4 py-1 rounded-full border border-gray-500"
+              className="text-[7px] sm:text-[9px] text-gray-500 px-4 py-1 rounded-full border border-gray-500"
             >
               {tag}
             </span>
           ))}
         </div>
-        <LiaLongArrowAltRightSolid className="text-lg" />
+        <LiaLongArrowAltRightSolid className="text-base sm:text-lg" />
       </div>
     </div>
   );
